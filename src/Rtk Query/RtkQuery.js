f2e9a5774cc1d7b6addc2d2bd1ac17595e1e843d
Ishan Lathwal <Ishan.L@ascentbusiness.com>
2024-03-02 22:40:04 +0530

    backend url added

--- src/Rtk Query/RtkQuery.js
+++ src/Rtk Query/RtkQuery.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const Api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:2000/api/v1/",
+    baseUrl: "https://interview-backend-gq8s.onrender.com/api/v1/",
   }),
   reducerPath: "api",
 
